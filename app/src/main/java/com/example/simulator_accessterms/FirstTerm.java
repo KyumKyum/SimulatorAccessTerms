5dc8b3199b9fbb129433b99094a58003499baf83
iraira7777@naver.com <iraira7777@naver.com>
2020-07-08 01:33:11 +0900

    Simulator - Access Terms

---
+++ app/src/main/java/com/example/simulator_accessterms/FirstTerm.java
@@ -0,0 +1,95 @@
+package com.example.simulator_accessterms;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstTerm extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
+
+    private RadioButton buttonYesOne, buttonNoOne, buttonYesTwo, buttonNoTwo;
+    private RadioGroup group1, group2;
+    private Button nextButton, cancelButton;
+
+    private Boolean agree1, agree2;
+    private Boolean buttonAvailable;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.terms);
+
+        buttonYesOne = findViewById(R.id.button_yes_term1);
+        buttonYesTwo = findViewById(R.id.button_yes_term2);
+        buttonNoOne = findViewById(R.id.button_no_term1);
+        buttonNoTwo = findViewById(R.id.button_no_term2);
+        nextButton = findViewById(R.id.button_next);
+        cancelButton = findViewById(R.id.button_cancel);
+        group1 = findViewById(R.id.group_one);
+        group2 = findViewById(R.id.group_two);
+
+        nextButton.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+        group1.setOnCheckedChangeListener(this);
+        group2.setOnCheckedChangeListener(this);
+
+        agree1 = false;
+        agree2 = false;
+        buttonAvailable = false;
+
+        updateButton();
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.button_next:
+                if(!buttonAvailable){
+                    Toast.makeText(this, "모든 약관에 동의해야합니다!", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(this, "NEXT", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+            case R.id.button_cancel:
+                finish();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.button_yes_term1:
+                agree1 = true;
+                break;
+
+            case R.id.button_no_term1:
+                agree1 = false;
+                break;
+
+            case R.id.button_yes_term2:
+                agree2 = true;
+                break;
+
+            case R.id.button_no_term2:
+                agree2 = false;
+                break;
+        }
+        updateButton();
+    }
+
+    public void updateButton() {
+        if (agree1 && agree2) {
+            buttonAvailable = true;
+        } else {
+            buttonAvailable = false;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/simulator_accessterms/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.simulator_accessterms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.button);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,FirstTerm.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
